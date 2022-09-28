68e62e180ef5487ae6e1791b79e93aea7fa25c90
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2022-09-28 18:36:04 +0530

    sample changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const index = () => {
-  return <div className="bg-red-500 h-full w-full font-bold">hello world</div>;
+  return (
+    <div className="bg-red-500 h-full w-full font-bold">this is earth</div>
+  );
 };
 
 export default index;
