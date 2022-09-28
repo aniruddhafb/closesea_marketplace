f3f8a24159859ff372683f40ae1f90f6d26b2200
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-09-28 18:53:20 +0530

    aniruddha timepass change first

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const index = () => {
-  return <div className="bg-red-500 h-full w-full font-bold">this is mars</div>;
+  return (
+    <div className="bg-red-500 h-full w-full font-bold">
+      this is aniruddha okk
+    </div>
+  );
 };
 
 export default index;
