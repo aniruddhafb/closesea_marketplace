e31d8015a58694216c6bc6283f9fc137d60ff03d
Aniruddha <aniruddhavikharankarfb@gmail.com>
2022-09-28 18:44:55 +0530

    first aniruddha change

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const index = () => {
-  return (
-    <div className="bg-red-500 h-full w-full font-bold">this is earth</div>
-  );
+  return <div className="bg-red-500 h-full w-full font-bold">this is mars</div>;
 };
 
 export default index;
