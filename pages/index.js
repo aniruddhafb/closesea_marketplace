af3385b507e6a86fe2eaeec26f656bce69de0f50
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2022-09-29 11:07:32 +0530

    fixings

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import Homepage from "./home/Homepage";
 
 const index = () => {
   return (
-<<<<<<< HEAD
     <>
       <Homepage />
     </>
-=======
-    <div className="bg-red-500 h-full w-full font-bold">
-      this is aniruddha okk
-    </div>
->>>>>>> f3f8a24159859ff372683f40ae1f90f6d26b2200
   );
 };
 
